2c789a8632a932af85479e32446e7c7a121c0b6e
Patrick Clark <pac3013564@gmail.com>
2015-01-25 16:02:12 -0500

    Fixed/Added joystick buttons
    Fixed a problem where the cubicDrive button would be deleted because it
    was added inside the autogenerated code and we also added a resetGyro
    button to reset the 'true north' if needed to without having to reset
    the entire robot

--- src/org/usfirst/frc1073/robot15/subsystems/DriveTrain.java
+++ src/org/usfirst/frc1073/robot15/subsystems/DriveTrain.java
@@ -109,6 +109,12 @@ public class DriveTrain extends Subsystem
 	
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+        
+    }
+    
+    public void resetGyro() 
+    {
+    	gyro.reset();
     }
     
 }
