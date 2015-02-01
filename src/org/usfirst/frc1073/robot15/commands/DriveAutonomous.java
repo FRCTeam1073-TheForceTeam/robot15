46a3fa02e0488e70d68715dd41925392bffe1e3e
Keenan Troddyn <keenan.troddyn@gmail.com>
2015-02-01 12:55:41 -0500

    added ultrasonic to autonomous

--- src/org/usfirst/frc1073/robot15/commands/DriveAutonomous.java
+++ src/org/usfirst/frc1073/robot15/commands/DriveAutonomous.java
@@ -35,16 +35,29 @@ public class  DriveAutonomous extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	
     	Robot.driveTrain.move(0.0f, driveSpeed, 0.0f);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        boolean isIt = false;
+    	
+    	if(isTimedOut())
+        {
+        	isIt = true;;
+        }
+        if(Robot.rangeFinder.getDistance() > 25)
+        {
+        	isIt = true;
+        }
+        return isIt;
     }
 
     // Called once after isFinished returns true
