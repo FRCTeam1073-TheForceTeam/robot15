5e0496482868eff33f939d3d748d529fbf04b623
nnapior <nnapior@gmail.com>
2015-02-10 15:49:36 -0500

    added comments and constant variables to clean up

--- src/org/usfirst/frc1073/robot15/commands/ToteElevatorScorePlatform.java
+++ src/org/usfirst/frc1073/robot15/commands/ToteElevatorScorePlatform.java
@@ -47,7 +47,6 @@ public class  ToteElevatorScorePlatform extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.elevator.resetMagPassed();
     }
 
     // Called when another command which requires one or more of the same
