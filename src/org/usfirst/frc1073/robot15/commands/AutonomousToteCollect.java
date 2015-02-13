51c075ca0545ed60c95a7914eed1268674953de3
Derek Wider <technoD@outlook.com>
2015-02-12 19:34:06 -0500

    Made sure autonomous commands dont run forever. Added method to elevator to get IrReadout

--- src/org/usfirst/frc1073/robot15/commands/AutonomousToteCollect.java
+++ src/org/usfirst/frc1073/robot15/commands/AutonomousToteCollect.java
@@ -35,11 +35,15 @@ public class  AutonomousToteCollect extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.toteCollector.wheelsCollect();
+    	if(Robot.elevator.irReadout() <= 2)
+    	{
+    		end();
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
