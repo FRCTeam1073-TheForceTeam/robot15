29db597394021d7eea038b4b59b21366b77fa34e
Greg Cowan <Cowang4@yahoo.com>
2015-02-16 13:09:49 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	sysProps.xml

--- src/org/usfirst/frc1073/robot15/commands/SendToDashboard.java
+++ src/org/usfirst/frc1073/robot15/commands/SendToDashboard.java
@@ -35,6 +35,7 @@ public class  SendToDashboard extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.dashboard.sendData();
     }
 
     // Make this return true when this Command no longer needs to run execute()
